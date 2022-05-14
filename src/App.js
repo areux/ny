280cdd6e2bd6a7026606f293c150679e0bd39780
xnix <x@nix>
2022-05-14 18:37:59 +0000

    cleanup

--- src/App.js
+++ src/App.js
@@ -39,64 +39,87 @@ const Spacer = styled.div({
   width: '40px',
 });
 
+function XHeader() {
+  return (
+    <Header>
+      <IconButton rounded text={false} size="large">
+        <Icon path={mdiChevronLeft} size={1} />
+      </IconButton>
+      <HeaderRightSide>
+        <IconButton rounded text={false} size="large">
+          <Icon path={mdiViewList} size={1} />
+        </IconButton>
+        <IconButton rounded text={false} size="large">
+          <Icon path={mdiBellBadge} size={1} />
+        </IconButton>
+      </HeaderRightSide>
+    </Header>
+  );
+}
+
+function XUserPrev(props) {
+  return (
+    <div>
+      <H4>{props.name}</H4>
+      <Subtitle2>{props.phone}</Subtitle2>
+    </div>
+  );
+}
+
+function XAvatar(props) {
+  return (
+    <div className="outer">
+      <div className='inner z1'>
+        <ProgressCircular
+          className='wx'
+          flat
+          color='green'
+          rotate={270}
+          width={3}
+          size={250}
+          value={props.value} />
+      </div>
+      <div className='inner-circle z2'></div>
+      <div className='inner z3'>
+        <Avatar
+          alt="x"
+          size={180}
+          src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+        />
+      </div>
+      <div className='inner-cc z4'>Score</div>
+    </div>
+  );
+}
+
+function XStats(props) {
+  return (
+    <div>
+      <FlexRow>
+        <FlexCol>
+          <H4>{props.skills}</H4>
+          <Subtitle2>Skills</Subtitle2>
+        </FlexCol>
+        <FlexCol>
+          <H4>{props.income / (1000 * 1000)} Million</H4>
+          <Subtitle2>Income</Subtitle2>
+        </FlexCol>
+        <FlexCol>
+          <H4>{props.works}</H4>
+          <Subtitle2>Works</Subtitle2>
+        </FlexCol>
+      </FlexRow>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <Header>
-        <IconButton rounded text={false} size="large">
-          <Icon path={mdiChevronLeft} size={1} />
-        </IconButton>
-        <HeaderRightSide>
-          <IconButton rounded text={false} size="large">
-            <Icon path={mdiViewList} size={1} />
-          </IconButton>
-          <IconButton rounded text={false} size="large">
-            <Icon path={mdiBellBadge} size={1} />
-          </IconButton>
-        </HeaderRightSide>
-      </Header>
-
-      <div className="outer">
-        <div className='inner z1'>
-          <ProgressCircular
-            className='wx'
-            flat
-            color='green'
-            rotate={270}
-            width={3}
-            size={250}
-            value={70} />
-        </div>
-        <div className='inner-circle z2'></div>
-        <div className='inner z3'>
-          <Avatar
-            alt="x"
-            size={180}
-            src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-          />
-        </div>
-        <div className='inner-cc z4'>Score</div>
-      </div>
-
-      <H4>Name</H4>
-      <Subtitle2>09131231231</Subtitle2>
-
-      <div>
-        <FlexRow>
-          <FlexCol>
-            <H4>6</H4>
-            <Subtitle2>Skills</Subtitle2>
-          </FlexCol>
-          <FlexCol>
-            <H4>1 Million</H4>
-            <Subtitle2>Income</Subtitle2>
-          </FlexCol>
-          <FlexCol>
-            <H4>1234</H4>
-            <Subtitle2>Works</Subtitle2>
-          </FlexCol>
-        </FlexRow>
-      </div>
+      <XHeader />
+      <XAvatar value={70} />
+      <XUserPrev name="Mehdi" phone="09131231231" />
+      <XStats skills={6} income={1000000} works={1234} />
 
       <FlexRow>
         <Button text size='large'>Change Profile</Button>
